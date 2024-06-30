f07b9c0dbf11cf706ccf09e0ad8e5213cf32617c
ShekharDan <btech10060.21@bitmesra.ac.in>
2024-06-30 21:00:05 +0530

    First Commit, just the boilerplate after spring initialisation.

---
+++ IndieHub/src/main/java/com/ecommerce/IndieHub/IndieHubApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.IndieHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndieHubApplication.class, args);
+	}
+
+}
---
+++ IndieHub/src/test/java/com/ecommerce/IndieHub/IndieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.IndieHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
